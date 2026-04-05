51e6e1850568bea24fd023b534d696f43c435d62
Sudhir Wani <sudhirwani@gmail.com>
2026-04-05 17:26:24 +0530

    backend4

--- backend/app.py
+++ backend/app.py
@@ -1,8 +1,10 @@
 from flask import Flask, request, jsonify
+from flask_cors import CORS
 import psycopg2
 import os
 
 app = Flask(__name__)
+CORS(app)
 
 conn = psycopg2.connect(os.getenv("DATABASE_URL"))
 
